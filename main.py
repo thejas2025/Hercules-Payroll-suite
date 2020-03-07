c02c9fd73ecff1f01708e77533b21378c7f93b89
thejas17 <thejas1705@hotmail.com>
2020-03-07 12:21:20 +0530

    Created Signup form for HRs. Default routing to home,about,signup,login pages are done.

--- main.py
+++ main.py
@@ -8,3 +8,14 @@ app = Flask(__name__)
 def homepage():
     return render_template('index.html')
 
+@app.route('/about/')
+def aboutpage():
+    return render_template('about.html')
+
+@app.route('/signup/')
+def signUpPageForHRs():
+    return render_template('signup.html')
+
+@app.route('/login/')
+def loginPageForHRs():
+    return render_template('login.html')
